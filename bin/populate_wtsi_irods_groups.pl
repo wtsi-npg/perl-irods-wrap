ceba4e4951cfc9ad638574ab7100c5f1e1aa8f99
David K Jackson <david.jackson@sanger.ac.uk>
2015-03-31 22:42:04 +0100

    avoid uninitialized value in pattern match

--- bin/populate_wtsi_irods_groups.pl
+++ bin/populate_wtsi_irods_groups.pl
@@ -58,7 +58,8 @@ my$rs=$s->resultset(q(CurrentStudy));
 my($group_count,$altered_count)= (0,0);
 while (my$st=$rs->next){
   my$study_id=$st->internal_id;
-  my@g= $st->data_access_group() =~ m/\S+/smxg;
+  my$gs=$st->data_access_group();
+  my@g= defined $gs ? $gs=~m/\S+/smxg : ();
   my$is_seq=($st->npg_information->count||$st->npg_plex_information->count)>0;
   my@m=@g      ? map{ _uid_to_irods_uid($_) } map { ug2id($_) } @g :
        $is_seq ? @public :

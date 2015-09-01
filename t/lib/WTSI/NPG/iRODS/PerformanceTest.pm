39f45255ac8db01d1914e23e1f84ae77e6dc0433
Keith James <kdj@sanger.ac.uk>
2015-09-01 11:47:49 +0100

    Limited the performance tests to be run only on selected iRODS zones.
    Made the performance limits more lenient.

--- t/lib/WTSI/NPG/iRODS/PerformanceTest.pm
+++ t/lib/WTSI/NPG/iRODS/PerformanceTest.pm
@@ -6,6 +6,7 @@ use warnings;
 use Benchmark qw(:all);
 use English qw(-no_match_vars);
 use File::Temp qw(tempdir);
+use List::AllUtils qw(any);
 
 use base qw(Test::Class);
 use Test::More tests => 9;
@@ -16,6 +17,17 @@ my $irods_tmp_coll;
 
 my $fixture_counter = 0;
 my $pid = $PID;
+
+my @test_hosting_zones = qw(tempZone testZone Sanger1-dev);
+my $can_test = 0;
+
+sub is_test_zone {
+  my ($irods) = @_;
+
+  my $zone = $irods->find_zone_name($irods->working_collection);
+
+  return any { $_ eq $zone } @test_hosting_zones;
+}
 
 sub make_fixture : Test(setup) {
   my $irods = WTSI::NPG::iRODS->new(strict_baton_version => 0);
@@ -37,121 +49,136 @@ sub teardown : Test(teardown) {
 
 sub collection_operations : Test(3) {
   my $irods = WTSI::NPG::iRODS->new(strict_baton_version => 0);
-  my $num_objs = 1000;
 
-  my $tmpdir = tempdir(CLEANUP => 1);
-  foreach my $i (1 .. $num_objs) {
-    my $file = "$tmpdir/$i";
-    open my $out, '>', $file or die "Failed to open $file for writing: $!\n";
-    print $out $i, "\n";
-    close $out;
-  }
+ SKIP: {
+     if (not is_test_zone($irods)) {
+       skip "Skipping test because this is not a iRODS test zone", 3;
+     }
 
-  my $coll = $irods->put_collection($tmpdir, $irods_tmp_coll);
-  foreach my $attr (qw(a b c)) {
-    foreach my $value (qw(x y)) {
-      my $units = $value eq 'x' ? 'cm' : undef;
-      $irods->add_collection_avu($coll, $attr, $value, $units);
-    }
-  }
+     my $num_objs = 1000;
 
-  # See 'perldoc Benchmark'
-  my @timestr_args = ('noc');
-  my $n = 5;
+     my $tmpdir = tempdir(CLEANUP => 1);
+     foreach my $i (1 .. $num_objs) {
+       my $file = "$tmpdir/$i";
+       open my $out, '>', $file or
+         die "Failed to open $file for writing: $!\n";
+       print $out $i, "\n";
+       close $out;
+     }
 
-  my $is_coll = timethis($n, sub { $irods->is_collection($coll) });
-  ok($is_coll->real < 2, "list_collection < 2s");
-  diag 'is_collection: ', timestr($is_coll, @timestr_args);
+     my $coll = $irods->put_collection($tmpdir, $irods_tmp_coll);
+     foreach my $attr (qw(a b c)) {
+       foreach my $value (qw(x y)) {
+         my $units = $value eq 'x' ? 'cm' : undef;
+         $irods->add_collection_avu($coll, $attr, $value, $units);
+       }
+     }
 
-  my $list_coll = timethis($n, sub { $irods->list_collection($coll) });
-  ok($list_coll->real < 5, "list_collection < 5s");
-  diag 'list_collection: ', timestr($list_coll, @timestr_args);
+     # See 'perldoc Benchmark'
+     my @timestr_args = ('noc');
+     my $n = 5;
 
-  my $coll_checksums =
-    timethis($n, sub { $irods->collection_checksums($coll) });
-  ok($coll_checksums->real < 120, "collection_checksums < 120s");
-  diag 'collection_checksums: ', timestr($coll_checksums, @timestr_args);
+     my $is_coll = timethis($n, sub { $irods->is_collection($coll) });
+     ok($is_coll->real < 5, "list_collection < 5s");
+     diag 'is_collection: ', timestr($is_coll, @timestr_args);
+
+     my $list_coll = timethis($n, sub { $irods->list_collection($coll) });
+     ok($list_coll->real < 10, "list_collection < 10s");
+     diag 'list_collection: ', timestr($list_coll, @timestr_args);
+
+     my $coll_checksums =
+       timethis($n, sub { $irods->collection_checksums($coll) });
+     ok($coll_checksums->real < 180, "collection_checksums < 180s");
+     diag 'collection_checksums: ', timestr($coll_checksums, @timestr_args);
+   }
 }
 
 sub object_operations : Test(6) {
   my $irods = WTSI::NPG::iRODS->new(strict_baton_version => 0);
-  my $num_objs = 100;
 
-  my $tmpdir = tempdir(CLEANUP => 1);
-  foreach my $i (1 .. $num_objs) {
-    my $file = "$tmpdir/$i";
-    open my $out, '>', $file or die "Failed to open $file for writing: $!\n";
-    print $out $i, "\n";
-    close $out;
-  }
+ SKIP: {
+    if (not is_test_zone($irods)) {
+      skip "Skipping test because this is not a iRODS test zone", 3;
+    }
 
-  my $coll = $irods->put_collection($tmpdir, $irods_tmp_coll);
-  my ($objs, $colls) = $irods->list_collection($coll);
-  foreach my $obj (@$objs) {
-    foreach my $attr (qw(a b c)) {
-      foreach my $value (qw(x y)) {
-        my $units = $value eq 'x' ? 'cm' : undef;
-        $irods->add_object_avu($obj, $attr, $value, $units);
+    my $num_objs = 100;
+
+    my $tmpdir = tempdir(CLEANUP => 1);
+    foreach my $i (1 .. $num_objs) {
+      my $file = "$tmpdir/$i";
+      open my $out, '>', $file or die "Failed to open $file for writing: $!\n";
+      print $out $i, "\n";
+      close $out;
+    }
+
+    my $coll = $irods->put_collection($tmpdir, $irods_tmp_coll);
+    my ($objs, $colls) = $irods->list_collection($coll);
+    foreach my $obj (@$objs) {
+      foreach my $attr (qw(a b c)) {
+        foreach my $value (qw(x y)) {
+          my $units = $value eq 'x' ? 'cm' : undef;
+          $irods->add_object_avu($obj, $attr, $value, $units);
+        }
       }
     }
+
+    my @timestr_args = ('noc');
+    my $n = 5;
+
+    my $is_obj =
+      timethis($n, sub {
+                 foreach my $i (1 .. $num_objs) {
+                   $irods->is_object("$coll/$i");
+                 }
+               });
+    ok($is_obj->real < 10, "is_object $num_objs < 10s");
+    diag 'is_object:', timestr($is_obj, @timestr_args);
+
+    my $list_obj =
+      timethis($n, sub {
+                 foreach my $i (1 .. $num_objs) {
+                   $irods->list_object("$coll/$i");
+                 }
+               });
+    ok($list_obj->real < 10, "list_object $num_objs < 10s");
+    diag 'list_object:', timestr($list_obj, @timestr_args);
+
+    my $get_obj_perms =
+      timethis($n, sub {
+                 foreach my $i (1 .. $num_objs) {
+                   $irods->get_object_permissions("$coll/$i");
+                 }
+               });
+    ok($get_obj_perms->real < 30, "get_object_permissions $num_objs < 30s");
+    diag 'get_object_permissions: ', timestr($get_obj_perms, @timestr_args);
+
+    my $get_obj_groups =
+      timethis($n, sub {
+                 foreach my $i (1 .. $num_objs) {
+                   $irods->get_object_groups("$coll/$i");
+                 }
+             });
+    ok($get_obj_groups->real < 30, "get_object_groups $num_objs < 30s");
+    diag 'get_object_groups: ', timestr($get_obj_groups, @timestr_args);
+
+    my $get_obj_meta =
+      timethis($n, sub {
+                 foreach my $i (1 .. $num_objs) {
+                   $irods->get_object_meta("$coll/$i");
+                 }
+               });
+    ok($get_obj_meta->real < 30, "get_object_meta $num_objs < 30s");
+    diag 'get_object_meta: ', timestr($get_obj_meta, @timestr_args);
+
+    my $find_objs_meta =
+      timethis($n, sub {
+                 foreach my $i (1 .. $num_objs) {
+                   $irods->find_objects_by_meta($coll, ['a' => 'x']);
+                 }
+               });
+    ok($find_objs_meta->real < 60, "find_objects_by_meta $num_objs < 60s");
+    diag 'find_objects_by_meta: ', timestr($find_objs_meta, @timestr_args);
   }
-
-  my @timestr_args = ('noc');
-  my $n = 5;
-
-  my $is_obj =
-    timethis($n, sub {
-               foreach my $i (1 .. $num_objs) {
-                 $irods->is_object("$coll/$i");
-               }
-             });
-  ok($is_obj->real < 5, "is_object $num_objs < 5s");
-  diag 'is_object:', timestr($is_obj, @timestr_args);
-
-  my $list_obj =
-    timethis($n, sub {
-               foreach my $i (1 .. $num_objs) {
-                 $irods->list_object("$coll/$i");
-               }
-             });
-  ok($list_obj->real < 5, "list_object $num_objs < 5s");
-  diag 'list_object:', timestr($list_obj, @timestr_args);
-
-  my $get_obj_perms =
-    timethis($n, sub {
-               foreach my $i (1 .. $num_objs) {
-                 $irods->get_object_permissions("$coll/$i");
-               }
-             });
-  ok($get_obj_perms->real < 15, "get_object_permissions $num_objs < 15s");
-  diag 'get_object_permissions: ', timestr($get_obj_perms, @timestr_args);
-
-  my $get_obj_groups =
-     timethis($n, sub {
-               foreach my $i (1 .. $num_objs) {
-                 $irods->get_object_groups("$coll/$i");
-               }
-             });
-  ok($get_obj_groups->real < 30, "get_object_groups $num_objs < 30s");
-  diag 'get_object_groups: ', timestr($get_obj_groups, @timestr_args);
-
-  my $get_obj_meta =
-    timethis($n, sub {
-               foreach my $i (1 .. $num_objs) {
-                 $irods->get_object_meta("$coll/$i");
-               }
-             });
-  ok($get_obj_meta->real < 30, "get_object_meta $num_objs < 30s");
-  diag 'get_object_meta: ', timestr($get_obj_meta, @timestr_args);
-
-  my $find_objs_meta =
-    timethis($n, sub {
-               foreach my $i (1 .. $num_objs) {
-                 $irods->find_objects_by_meta($coll, ['a' => 'x']);
-               }
-             });
-  ok($find_objs_meta->real < 30, "find_objects_by_meta $num_objs < 30s");
-  diag 'find_objects_by_meta: ', timestr($find_objs_meta, @timestr_args);
 }
 
 1;
